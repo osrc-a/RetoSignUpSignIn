ea789e9b5a3ee6e31546a9f73e9fa54bc2fbbb3f
markel0707 <markelarabiourrutia@gmail.com>
2024-10-10 13:17:15 +0200

    subidaMarkelClientSocket

--- SignInSignUpClient/src/userinterfacetier/SignUpFXMLController.java
+++ SignInSignUpClient/src/userinterfacetier/SignUpFXMLController.java
@@ -5,50 +5,51 @@
  */
 package userinterfacetier;
 
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import modelo.FactorySignable;
 import modelo.Usuario;
-
 
 /**
  *
  * @author 2dam
  */
+public class SignUpFXMLController {
 
-public class SignUpFXMLController{
-    
     @FXML
     private TextField nombre;
-    
+
     @FXML
     private TextField apellido;
-    
+
     @FXML
     private TextField email;
-    
+
     @FXML
     private TextField contrasena;
-    
+
     @FXML
     private TextField edad;
-    
-    
-    
-    private void registro(ActionEvent event){
-        Usuario usu=new Usuario();
-        usu.setNombre(nombre.getText());
-        usu.setApellido(apellido.getText());
-        usu.setEmail(email.getText());
-        
-        
-        
-    }
 
+    private void registro(ActionEvent event) {
+
+        try {
+            Usuario usu = new Usuario();
+            usu.setNombre(nombre.getText());
+            usu.setApellido(apellido.getText());
+            usu.setEmail(email.getText());
+            FactorySignable.getSignable().registrar(usu);
+        } catch (Exception ex) {
+            Logger.getLogger(SignUpFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 
 }
