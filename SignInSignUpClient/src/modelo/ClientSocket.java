23dff68133e237099ac7970bba1c98f939f20f4a
2dam <2dam@HZ375758.TartangaLH.eus>
2024-10-25 13:06:41 +0200

    Merge origin/melany into melany
    Conflicts:
    	RetoSignInSignUpLibraries/src/modelo/Signable.java

--- SignInSignUpClient/src/modelo/ClientSocket.java
+++ SignInSignUpClient/src/modelo/ClientSocket.java
@@ -53,5 +53,6 @@ public class ClientSocket implements Signable {
     public void login(Usuario user) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+}
 
 
